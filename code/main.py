85036a7a6320d05041bcd30ac54d1057aba65081
nerazull <66993587+nerazull@users.noreply.github.com>
2024-03-03 11:43:59 +0200

    Level setup (Lesson 126): - Created setup method in Main class for importing assets - Created Tile class

--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
 import pygame, sys
 from settings import *
+from pytmx.util_pygame import load_pygame
+from tile import Tile
 
 class Main:
 	def __init__(self):
@@ -7,6 +9,16 @@ class Main:
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 		pygame.display.set_caption("Contra")
 		self.clock = pygame.time.Clock()
+
+		# groups
+		self.all_sprites = pygame.sprite.Group()
+
+		self.setup()
+
+	def setup(self):
+		tmx_map = load_pygame('../data/map.tmx')
+		for x, y, surf in tmx_map.get_layer_by_name('Level').tiles():
+			Tile((x * 0,y * 0), surf, self.all_sprites)
 
 	def run(self):
 		while True:
@@ -16,8 +28,10 @@ class Main:
 					pygame.quit()
 					sys.exit()
 			dt = self.clock.tick() / 1000
-
 			self.display_surface.fill((249,131,103))
+
+			self.all_sprites.update(dt)
+			self.all_sprites.draw(self.display_surface)
 
 			pygame.display.update()
 
