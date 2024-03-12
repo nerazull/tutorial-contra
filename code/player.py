47aafd6d36b8cf1a94989d0aa91b8b65404d6870
nerazull <66993587+nerazull@users.noreply.github.com>
2024-03-12 17:08:41 +0200

    Starting the player (Lesson 127): - Created Player and AllSprites class - Added input, movement and camera

---
+++ code/player.py
@@ -0,0 +1,49 @@
+import pygame
+from settings import *
+from pygame.math import Vector2 as vector
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, pos, groups):
+		super().__init__(groups)
+		self.image = pygame.Surface((40,80))
+		self.image.fill('yellow')
+		self.rect = self.image.get_rect(topleft = pos)
+
+		# float based movement
+		self.pos = vector(self.rect.topleft)
+		self.direction = vector()
+		self.speed = 200
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		# horizontal input
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+			
+		# vertical input
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+		elif keys[pygame.K_DOWN]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
+
+	def move(self,dt):
+		# horizontal movement + collision
+		self.pos.x += self.direction.x * self.speed * dt
+		self.rect.x = round(self.pos.x)
+		
+
+		# vertical movement + collision
+		self.pos.y += self.direction.y * self.speed * dt
+		self.rect.y = round(self.pos.y)
+		
+
+	def update(self,dt):
+		self.input()
+		self.move(dt)
