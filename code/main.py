f4fbe4ebd6ee760208fadb0a2a5afedd90110abf
nerazull <66993587+nerazull@users.noreply.github.com>
2024-03-14 17:15:41 +0200

    Bullets 1 (Lesson 134): - Imported bullet asset - Created Bullet class - Created Bullet object inside shoot method within Main class - Implemented timer for shooting logic in Player class

--- code/main.py
+++ code/main.py
@@ -4,6 +4,7 @@ from pytmx.util_pygame import load_pygame
 from tile import Tile, CollisionTile, MovingPlatform
 from player import Player
 from pygame.math import Vector2 as vector
+from bullet import Bullet
 
 class AllSprites(pygame.sprite.Group):
 	def __init__(self):
@@ -35,8 +36,12 @@ class Main:
 		self.all_sprites = AllSprites()
 		self.collision_sprites = pygame.sprite.Group()
 		self.platform_sprites = pygame.sprite.Group()
+		self.bullet_sprites = pygame.sprite.Group()
 
 		self.setup()
+
+		# bullet images
+		self.bullet_surf = pygame.image.load('../graphics/bullet.png').convert_alpha()
 
 	def setup(self):
 		tmx_map = load_pygame('../data/map.tmx')
@@ -53,7 +58,7 @@ class Main:
 		# objects
 		for obj in tmx_map.get_layer_by_name('Entities'):
 			if obj.name == 'Player':
-				self.player = Player((obj.x,obj.y), self.all_sprites, '../graphics/player', self.collision_sprites)
+				self.player = Player((obj.x,obj.y), self.all_sprites, '../graphics/player', self.collision_sprites, self.shoot)
 
 		self.platform_border_rects = []
 		for obj in tmx_map.get_layer_by_name('Platforms'):
@@ -80,6 +85,8 @@ class Main:
 				platform.pos.y = platform.rect.y
 				platform.direction.y = -1
 
+	def shoot(self, pos, direction, entity):
+		Bullet(pos, self.bullet_surf, direction, [self.all_sprites, self.bullet_sprites])
 
 	def run(self):
 		while True:
